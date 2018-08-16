a56fcfe8e2f6a7b4ed352d60d93090edbc87b510
aramaki <reepn.bump@gmail.com>
2018-08-16 21:07:50 +0900

    最初のコミット

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ) ?>">
+<title>日本の景色写真展</title>
+<link rel="stylesheet" href="css/common.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/responsive.css" media="screen and (max-width:480px)">
+<?php wp_head();?>
+</head>
+
+<body <?php body_class(); ?>>
+
+  <header>
+    <h1>
+      <a href="index.html">
+        <img src="images/logo.gif" width="268" height="64" alt="日本の景色写真展">
+      </a>
+    </h1>
+  </header>
+
+  <nav class="nav">
+    <ul>
+      <li><a href="introduction.html">展覧会について</a></li>
+      <li><a href="schedule.html">スケジュール</a></li>
+      <li><a href="gallery.html">作品紹介</a></li>
+      <li><a href="tickets.html">前売り券申し込み</a></li>
+    </ul>
+  </nav>
